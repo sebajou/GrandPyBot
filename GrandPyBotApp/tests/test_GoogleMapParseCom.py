6bd8754b98a6cc1865aeaefa045a0da1c2517047
sebajou <sebajou@gmail.com>
2020-05-29 13:05:03 +0200

    First TDD cycle on parser for Google Map API

---
+++ GrandPyBotApp/tests/test_GoogleMapParseCom.py
@@ -0,0 +1,41 @@
+import GrandPyBotApp.functions.GoogleMapParseCom as script
+import pytest
+
+
+class TestParse:
+
+    def setup_method(self):
+        self.sentence_to_parse1 = "Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"
+        self.sentence_to_parse2 = "Que peux-tu me dire à propos de la gare de Lyon à Paris ?"
+        self.sentence_to_parse3 = "Je voudrais aller au vieux port à Marseille, tu connais ?"
+        self.sentence_to_parse4 = "Ah ! Venise, c'est tellement romantique, j'aimerais beaucoup y aller. "
+        self.sentence_to_parse5 = "Cet été, je pars dans les Antilles. "
+        self.sentence_to_parse6 = "Ma femme veut qu'on parte en vacances en Tunisie. Elle veut qu'on aille dans un " \
+                                  "hôtel. "
+        self.sentence_to_parse7 = "Je veux partir pour Hong-Kong !"
+        self.sentence_to_parse8 = "GrandPyBot, dis-moi tout sur la Barbade. "
+        self.sentence_to_parse9 = "Est-ce que tu es allé au Timor oriental ? "
+
+    def test_parse_message_from_front(self):
+        message_to_parse1 = self.sentence_to_parse1
+        message_to_parse2 = self.sentence_to_parse2
+        message_to_parse3 = self.sentence_to_parse3
+        message_to_parse4 = self.sentence_to_parse4
+        message_to_parse5 = self.sentence_to_parse5
+        message_to_parse6 = self.sentence_to_parse6
+        message_to_parse7 = self.sentence_to_parse7
+        message_to_parse8 = self.sentence_to_parse8
+        message_to_parse9 = self.sentence_to_parse9
+        # Control that word in message_to_parse are present in a given list
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse1) if word in ["openclassrooms"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse2) if word in ["gare", "lyon", "paris"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse3) if word in ["vieux", "port", "marseille"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse4) if word in ["venise"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse5) if word in ["antilles"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse6) if word in ["tunisie"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse7) if word in ["hong-kong"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse8) if word in ["barbade"]]
+        assert [word for word in script.GoogleMapParseCom.parse_message_from_front(message_to_parse9) if word in ["timor", "oriental"]]
+
+    def test_send_message_to_api(self):
+        pass
--- README.md
+++ README.md
@@ -3,6 +3,32 @@
 This conversational bot will give you desciption of a location and a map to go there from an adresse. 
 This is a test driven project. 
 
-## Fonctionnality
+## Fonctionnalities
 
+### Static page for display
+Static HTML CSS page to display main information.
 
+### Dynamic page for display
+JavaScript / JQuery for modify body text with data query on back end.
+
+### Parser for Google Map and com with API 
+#### => Class GoogleMapParseCom
+Parser for Google Map with Python back end. 
+Speak with Google Map API with Python back end and Json message.
+
+### Parser for Media Wiki and com with API 
+#### => Class MediaWikiParseCom 
+Parser for Media Wiki with Python back end.
+Speak with Media Wiki API with Python back end and Json message.
+
+### Randomly sentence send
+#### => Class RandomSentence
+Engine to randomly send sentence from GranPapy with Python back end.
+
+### Json from API parser
+#### => Class JsonParser
+On back end Parse Json from API Google Map and Media wiki.
+
+### Back end and Front end component for Ajax message
+#### => Class AjaxMessage
+Ajax for data exchange from server to front, POST.
